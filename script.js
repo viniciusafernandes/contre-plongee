8eae839eb2021c5a8d8491113d1009d8367f9c74
Vinícius Augusto Fernandes da Silveira <viniaugusto817@gmail.com>
2024-10-31 19:20:18 -0300

    att

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-$(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
-  });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
-
 const box = document.querySelector(".container");
 const imagens = document.querySelectorAll(".container img");
 
